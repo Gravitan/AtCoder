1cb88642a964e3c17c51e7979d32234b88738653
Gravitan <150590461+Gravitan@users.noreply.github.com>
2026-04-29 14:56:14 +0900

    boot camp m

--- template.cpp
+++ template.cpp
@@ -11,5 +11,14 @@ int main() {
     cin.tie(nullptr);
     ll ans = 0;
     
-    
+    ll l, r; cin >> l >> r;
+    if (r - l < 2018) {
+        ans = 2019;
+        for (int i = l; i < r; ++i) {
+            for (int j = i + 1; j <= r; ++j) {
+                ans = min(ans, (ll) (i * j) % 2019);
+            }
+        }
+    }
+    cout << ans << endl;
 }
